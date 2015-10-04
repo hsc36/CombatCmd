6aeadc590acd5bcc4755dfd8bd576d4783883fed
hsc36 <hsc36@drexel.edu>
2015-10-04 08:22:19 -0400

    Basic methods for Deck actions Placeholders for load/save Deck and Player

--- Player.py
+++ Player.py
@@ -1,13 +1,12 @@
 import random
-from future import division
 
-import Card
+from Card import Location, Unit, Supply, Order
 
 # Deck
+# @NOTE: Dealing with Duplicate Cards While Saving Decks; Use a Counter?
 class Deck:
 	'A stack of Cards for Play'
-	cards = []
-	def __init__(*cards):
+	def __init__(self, *cards):
 		# Build Deck
 		## Open Deck File
 		## Construct Cards
@@ -15,49 +14,55 @@ class Deck:
 		#if not all(isinstance(card, Card) for card in cards):
 		#	# Pass Exception
 		# else:
-		self.cards = cards
+		self.cards = cards # The stack of cards (as a List from top to bottom)
+
+	# Load Deck from Disk
+	def load():
+
+	# Save Deck to Disk
+	def save():
 
 	# Shuffle
 	def shuffle(t):
-		[random.shuffle(cards) for _ in range(t)]
+		[random.shuffle(self.cards) for _ in range(t)]
 
 	# Draw
 	def draw():
-		return cards.pop(0)
+		return self.cards.pop(0)
 
 	# Re-deck - Bottom
 	def redeck_bottom(card):
-		cards.append(card)
+		self.cards.append(card)
 
 	# Re-deck - Top
 	def redeck_top(card):
-		cards.insert(0, card)
+		self.cards.insert(0, card)
 
 	# Reveal
 	def reveal(i):
-		return cards[i]
+		return self.cards[i]
 
 	# Select
 	def select(i):
-		return cards.pop(i)
+		return self.cards.pop(i)
+
+	# Add Card
+	# @NOTE: Use 'redeck_top'
+
+	# Remove Card
+	def remove(card):
+		self.cards.remove(card)
 
 # Player
 class Player:
-	'A player in the game'
-	id_code = ''
-	name = ''
-	decks = []
-	stats = {
-		'win':0,
-		'loss':0,
-		'draw':0,
-		'resign':0
-	}
+	def __init__(self, id_code, name, decks):
+		'A player in the game'
+		id_code = ''
+		name = ''
+		decks = []
 
-	# Games Played
-	def games_played():
-		return sum(stats.intervalues())
+	# Load Deck from Disk
+	def load():
 
-	# Win Percentage
-	def win_percent():
-		return 0 if win == 0 else return float('{:1.3f}'.format(stat['win'] / games_played()))
+	# Save Deck to Disk
+	def save():
