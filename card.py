7697a4ac02dd6a409629944977e79e9ce43d88c2
hsc36 <hsc36@drexel.edu>
2015-10-22 20:59:55 -0400

    Modified Deck Design
    Deck is now a container for cards (NOT Card IDs)
    Divided Deck methods into System, Construction, and Gameplay
    Added methods to return a list of Card IDs for each card in a Deck, for
    storage with the Player

--- card.py
+++ card.py
@@ -34,12 +34,12 @@ class Card(object):
 	# Load a Card, given its ID and the full dictionary of cards
 	@abstractmethod
 	def load(self, id_code, cards_dict):
-		return pickled.decode(cards_dict[id_code])
+		return pickle.decode(cards_dict[id_code])
 
 	# Load a Card, given its particular sub-dictionary
 	@abstractmethod
 	def load(self, card_dict):
-		return pickled.decode(card_dict)
+		return pickle.decode(card_dict)
 
 	@abstractmethod
 	def save(self):
@@ -113,3 +113,126 @@ class Order(Card):
 	def list_details(self):
 		return super(Order, self).list_details() + [
 			('Command', self.command)]
+
+# Deck
+# @NOTE_1: Dealing with Duplicate Cards While Saving Decks; Use Counter?
+class Deck:
+
+	'A container of Cards for Play'
+
+	def __init__(self, id_code=None, name=None, cards=None):
+		# Build Deck
+		## Open Deck File
+		## Construct Cards
+		## Compile Deck
+		if not all(isinstance(card, Card) for card in cards):
+			 raise ValueError('Deck can only contain Cards', [card for card in cards if not isinstance(card, Card)])
+		# else:
+		self.id_code = '' if id_code == None else id_code
+		self.name = '' if name == None else name
+		self.cards = [] if cards == None else cards # The stack of cards (as a List from top to bottom)
+
+	###---Load/Save---###
+	def sys(self):
+
+		'System (Load/Save) Methods'
+
+		# Make a Deck list from Cards in a Deck
+		# @NOTE_1
+		def list_card_ids(self):
+			return [card.id_code for card in self.cards]
+
+		# Make a list of Cards from Card IDs
+		# @NOTE_1
+		def from_list(self, card_ids):
+			return [Card.load(card_id) for card_id in card_ids]
+
+		# Make a list of Card IDs from a list of Cards
+		# @NOTE_1
+		def to_list(self):
+			return [card.id_code for card in list_card_ids()]
+
+		# Load a Deck
+		def load(self, deck):
+			self.id_code = deck.id_code
+			self.name = deck.name
+			self.cards = from_list(deck.cards)
+
+		# Store a Deck (for saving)
+		def store(self):
+			deck = Deck()
+			deck.id_code = self.id_code
+			deck.name = self.name
+			deck.cards = deck.to_list()
+			return pickle.encode(deck)
+
+	###---Deck Building---###
+	def build(self):
+
+		'Deck Construction'
+
+		# Add Cards
+		def add(*card_ids):
+			if not isinstance(card_ids, list):
+				card_ids = list(card_ids)
+			self.cards.extend(card_ids)
+
+		# Add 'x' amount of a Card for multiple Cards
+		def robust_add(**card_ids_amounts):
+			if not isinstance(card_ids_amounts, dict):
+				card_ids_amounts = dict(card_ids_amounts)
+			[add(card_id) for _ in xrange(amount) for card_id, amount in card_ids_amounts]
+
+		# Remove Cards
+		def remove(*card_ids):
+			if not isinstance(card_ids, list):
+				card_ids = list(card_ids)
+			[self.cards.remove(card_id) for card_id in card_ids]
+
+		# Remove 'x' amount of a Card for multiple Cards
+		def robust_remove(**card_ids_amounts):
+			if not isinstance(card_ids_amounts, dict):
+				card_ids_amounts = dict(card_ids_amounts
+			[remove(card_id) for _ in xrange(amount) for card_id, amount in card_ids_amounts]
+
+	###---Gameplay---###
+	def game(self):
+
+		'Gameplay Methods'
+
+		# Shuffle
+		def shuffle(self, t):
+			[random.shuffle(self.cards) for _ in range(t)]
+
+		# Draw
+		def draw(self):
+			return self.cards.pop(0)
+
+		# Re-deck - Bottom
+		def redeck_bottom(self, cards):
+			if not isinstance(cards, list):
+				card = [cards]
+			[self.cards.append(card) for card in cards]
+
+		# Re-deck - Top
+		def redeck_top(self, cards):
+			if not isinstance(cards, list):
+				card = [cards]
+			[self.cards.insert(0, card) for card in cards]
+
+		# Reveal
+		def reveal(self, card):
+			return self.cards[card]
+
+		# Select
+		def select(self, card):
+			return self.cards.pop(card)
+
+		# Add Card
+		# @NOTE: Use 'redeck_top'
+
+		# Remove Card
+		def remove(self, cards):
+			if not isinstance(cards, list):
+				card = [cards]
+			[self.cards.remove(card) for card in cards]
