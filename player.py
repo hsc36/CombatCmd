c171f170bac76f61319b2d1b4ef40c6ce19689fa
hsc36 <hsc36@drexel.edu>
2015-10-22 20:56:31 -0400

    Separated Deck from Player
    Made Deck a container for Cards, instead of an attribute of Player

--- player.py
+++ player.py
@@ -2,79 +2,7 @@ import json
 import random
 import jsonpickle as pickle
 
-from card import *
-
-# Deck
-# @NOTE: Dealing with Duplicate Cards While Saving Decks; Use a Counter?
-# Separate Deck and DeckList into seperate objects (one stores cards for game one stores card_ids for file saving)
-class Deck:
-
-	'A container of Cards for Play'
-
-	def __init__(self, id_code=None, name=None, cards=None):
-		# Build Deck
-		## Open Deck File
-		## Construct Cards
-		## Compile Deck
-		if not all(isinstance(card, Card) for card in cards):
-			 raise ValueError('Deck can only contain Cards', [card for card in cards if not isinstance(card, Card)])
-		# else:
-		self.id_code = '' if id_code == None else id_code
-		self.name = '' if name == None else name
-		self.cards = [] if cards == None else cards # The stack of cards (as a List from top to bottom)
-
-	# Load Deck from Disk
-	def load(self, card_ids):
-		if not isinstance(card_ids, list):
-			card_ids = [card_ids]
-		self.cards = [Card.load(card_id) for card_id in card_ids]
-
-	# Save Deck to Disk
-	# @NOTE: Instead, create a list of Card IDs and the number of duplicates
-	def save(self):
-		card_ids = [card.id_code for card in cards]
-		self.cards = card_ids
-
-	# Get Card List
-	def list_card_ids(self):
-		return [card.id_code for card in cards]
-
-	# Shuffle
-	def shuffle(self, t):
-		[random.shuffle(self.cards) for _ in range(t)]
-
-	# Draw
-	def draw(self):
-		return self.cards.pop(0)
-
-	# Re-deck - Bottom
-	def redeck_bottom(self, cards):
-		if not isinstance(cards, list):
-			card = [cards]
-		[self.cards.append(card) for card in cards]
-
-	# Re-deck - Top
-	def redeck_top(self, cards):
-		if not isinstance(cards, list):
-			card = [cards]
-		[self.cards.insert(0, card) for card in cards]
-
-	# Reveal
-	def reveal(self, card):
-		return self.cards[card]
-
-	# Select
-	def select(self, card):
-		return self.cards.pop(card)
-
-	# Add Card
-	# @NOTE: Use 'redeck_top'
-
-	# Remove Card
-	def remove(self, cards):
-		if not isinstance(cards, list):
-			card = [cards]
-		[self.cards.remove(card) for card in cards]
+from card import Deck
 
 # Player
 class Player:
@@ -92,25 +20,30 @@ class Player:
 	# Load Deck from Disk
 	def load(self, id_code):
 		with open(__directory + '/' + id_code + '.' + __file_extension) as player_file:
-			player = json.load(player_file)
-			self.id_code = player['id_code']
-			self.name = player['name']
-			self.decks = [Deck.load(deck) for deck in player['decks']]
+			player = pickle.decode(player_file)
+			self.id_code = player.id_code
+			self.name = player.name
+			self.decks = [Deck.from_list(deck) for deck in player.decks]
 
 	# Save Deck to Disk
 	def save(self):
 		with open(__directory + '/' + id_code + '.' + __file_extension, 'w') as player_file:
-			player = {}
-			player['id_code'] = self.id_code
-			player['name'] = self.name
-			player['decks'] = Deck.save(decks)
+			pickle.encode(self)
 
+	# Create a new Player
 	def new(self, name):
-		self.id_code = generate_player_id(name) #@TODO: ID Generator
+		self.id_code = generate_player_id(name) #@TODO: ID Generator, based on type()
 		self.name = name
 		self.decks = []
 
-
-	# List Decks
+	# List Deck Names
 	def list_deck_names(self):
-		return [deck.name for deck in decks]
+		return [deck.name for deck in self.decks]
+
+	# List Deck IDs
+	def list_deck_ids(self):
+		return [deck.id_code for deck in self.decks]
+
+	# Add a Deck
+
+	# Remove a Deck
